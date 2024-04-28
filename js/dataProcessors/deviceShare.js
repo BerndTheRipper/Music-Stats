2be872ea89e01d6982d238d4337965fb2c37dcfb
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2024-04-28 23:57:36 +0200

    added deviceShare and processor logic

--- js/dataProcessors/deviceShare.js
+++ js/dataProcessors/deviceShare.js
@@ -2,26 +2,121 @@ import Processor from "./processor.js"
 
 /**
  * @class
+ * @hideconstructor
  * @classdesc Draws a pie chart that shows the share of devices that I streamed from over a global lifespan
  * @extends Processor
  * @todo figure out how to solve this issue with the await not being available in the constructor, maybe make the constructor static
  */
 export default class DeviceShare extends Processor {
+	platformStarts = [
+		"Other",
+		"Windows",
+		"iOS",
+		"Android OS",
+		"Android-tablet",
+		"web_player windows"
+	];
+	platformLabels = [
+		"Other",
+		"Windows in-app",
+		"iOS",
+		"Android OS",
+		"Android-tablet",
+		"Browser windows"
+	];
+	totalStreams = 0;
 	/**
-	 * 
+	 * Don't use! Use [createProcessor]{@link DeviceShare.createProcessor} instead.
+	 * @throws {SyntaxError} If this function gets called directly
+	 */
+	constructor() {
+		super();
+	}
+
+	/**
+	 * draws new pie chart representing where I streamed how many songs
+	 */
+	async drawChart() {
+		await this.readFiles();
+		this.cleanupData();
+		this.setUpChart();
+
+		this.chart.update();
+	}
+
+	async readFiles() {
+		for await (const fileHandle of this.neededFolderHandle.values()) {
+			if (!fileHandle.name.startsWith("endsong_")) continue;
+			let file = await fileHandle.getFile();
+
+			let entriesObject = JSON.parse(await file.text());
+
+			let statsObtained = this.readStats(entriesObject);
+			this.statsToShow.addData(statsObtained);
+		}
+	}
+
+	readStats(dataObject) {
+		let output = {};
+		let totalStreams = dataObject.length;
+		for (let entry of dataObject) {
+			let platformToList = "Other";
+
+			for (let starter of this.platformStarts) {
+				if (!entry.platform.startsWith(starter)) continue;
+				platformToList = starter;
+				break;
+			}
+
+			if (!output[platformToList]) {
+				output[platformToList] = 1;
+				continue;
+			}
+			output[platformToList] += 1;
+		}
+
+		return output;
+	}
+
+	/**
+	 * @todo move summarizing platforms here
+	 */
+	cleanupData() {
+		if (this.platformStarts.length != this.platformLabels.length) {
+			throw new Error("Platform starts and platform labels must be the same lengths.");
+		}
+
+		delete this.statsToShow.addData;
+
+		for (let i in this.platformLabels) {
+			if (this.platformStarts[i] == this.platformLabels[i]) continue;
+			this.statsToShow[this.platformLabels[i]] = this.statsToShow[this.platformStarts[i]];
+			delete this.statsToShow[this.platformStarts[i]];
+		}
+	}
+
+	setUpChart() {
+		let data = {
+			labels: Object.keys(this.statsToShow),
+			datasets: [
+				{
+					label: "dataset 1",
+					data: Object.values(this.statsToShow),
+				}
+			]
+		}
+
+		this.chart.config.type = "pie";
+		this.chart.config.data = data;
+	}
+
+	/**
 	 * @param {Chart} chart A pre-initialized chart object
 	 * @param {FileSystemDirectoryHandle} folder The folder at the root of myData
 	 */
-	constructor(chart, folder) {
-		super(chart, folder, "extended");
-		// for await (const fileHandle of this.neededFolderHandle.values()) {
-		// 	if (!fileHandle.name.startsWith("endsong_")) continue;
-		// 	let file = await fileHandle.getFile();
+	static async createProcessor(chart, folder) {
+		let output = await super.createProcessor(chart, folder, "extended");
 
-		// 	let entriesObject = JSON.parse(await file.text());
-
-		// 	let statsObtained = getPartialStats(entriesObject, true);
-		// 	calculations.addInHere(statsObtained);
-		// }
+		return output;
 	}
 }
--- js/dataProcessors/processor.js
+++ js/dataProcessors/processor.js
@@ -1,10 +1,91 @@
 /**
  * @class
+ * @hideconstructor
  * @classdesc Base class for all the other processors
  */
 export default class Processor {
+	/**
+	 * Gets set to true if the constructor is being called through justifiable means.
+	 */
+	static #initializing = false;
+	/**
+	 * The readily initialized chart
+	 */
+	chart;
+	/**
+	 * The folder handle of the folder that contains the necessary files
+	 */
 	neededFolderHandle;
-	statsToShow;
+	/**
+	 * An object that contains the stats that get shown on the chart
+	 */
+	statsToShow = {
+		addData: function (stuffToAdd) {
+			for (let [key, value] of Object.entries(stuffToAdd)) {
+				if (typeof value == "object") {
+					if (!this[key]) {
+						this[key] = { addData: this.addData };
+					}
+					this[key].addData(value);
+				}
+				else if (typeof value == "number") {
+					if (!this[key]) {
+						this[key] = value;
+						continue;
+					}
+					this[key] += value;
+				}
+			}
+		}
+	};
+
+	constructor() {
+		if (!Processor.#initializing) {
+			throw new SyntaxError("This constructor is private. Please use createProcessor instead.");
+		}
+		Processor.#initializing = false;
+	}
+
+	/**
+	 * @abstract
+	 * Draws the chart
+	 */
+	async drawChart() { }
+
+	/**
+	 * @abstract
+	 * reads the data from the files and provides data as object to [readStats]{@link Processor#readStats}
+	 */
+	async readFiles() { }
+
+	/**
+	 * @abstract
+	 * reads the needed stats from the object provided
+	 */
+	readStats(dataObject) { }
+
+	/**
+	 * @abstract
+	 * Cleans up the data so it looks nicer on the chart
+	 */
+	cleanupData() { }
+
+	/**
+	 * @abstract
+	 * changes the settings of the chart so that it will show our diagram. It does not make the chart update tho!
+	 */
+	setUpChart() { }
+
+	/**
+	 * Clears chart content
+	 */
+	clearChartConfig() {
+		this.chart.config.type = "";
+		this.chart.config.data = {};
+		this.chart.config.options = {};
+		// this.chart.config.plugins = [];
+	}
+
 	/**
 	 * Creates a new processor. This constructor makes type validations and 
 	 * @param {Chart} chart The chart that should be drawn into
@@ -12,7 +93,7 @@ export default class Processor {
 	 * @param {string} neededFolder The name of the data folder that we need
 	 * @throws {TypeError} if one of the parameters has an incorrect type
 	 */
-	constructor(chart, folder, neededFolder) {
+	static async createProcessor(chart, folder, neededFolder) {
 		if (!(chart instanceof Chart)) {
 			throw new TypeError("Chart parameter needs to be of type Chart (duh)");
 		}
@@ -25,15 +106,27 @@ export default class Processor {
 			throw new TypeError("neededFolder needs to be of type string");
 		}
 
+		Processor.#initializing = true;
+		let output = new this();
+		output.chart = chart;
+
 		if (folder.name == neededFolder) {
-			this.neededFolderHandle = folder;
+			output.neededFolderHandle = folder;
 		}
 		else {
 			for await (let folderHandle of folder.values()) {
 				if (!(folderHandle instanceof FileSystemDirectoryHandle)) continue;
 				if (folderHandle.name != neededFolder) continue;
-				this.neededFolderHandle = folderHandle;
+				output.neededFolderHandle = folderHandle;
+				break;
 			}
 		}
+
+		if (!(output.neededFolderHandle instanceof FileSystemDirectoryHandle)) {
+			throw new ReferenceError("Could not find the needed folder")
+		}
+		output.clearChartConfig();
+
+		return output;
 	}
 }
