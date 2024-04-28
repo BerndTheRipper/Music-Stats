b6337174257334e99dda23d4316b1b6d384994bc
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2024-04-28 23:58:40 +0200

    implemented dataProcessors and removed references to calculator.js

--- js/start.js
+++ js/start.js
@@ -2,6 +2,7 @@ import DeviceShare from "./dataProcessors/deviceShare.js"
 
 var dataFolder;
 var currentChart;
+var currentProcessor;
 
 //TODO use this to generate a list in the control panel
 const optionsIfAvailable = {
@@ -9,6 +10,10 @@ const optionsIfAvailable = {
 	"extended": ["deviceShare"],
 	"technical": [/* currently none */]
 };
+
+const diagramClasses = {
+	"deviceShare": DeviceShare
+}
 
 window.addEventListener("load", () => {
 	let dataSelector = document.querySelector("#dataSelector");
@@ -22,74 +27,29 @@ window.addEventListener("load", () => {
 
 async function requestNewDiagram(e) {
 	if (!e.isTrusted) {
-		throw new DOMException("Why is this not trusted?");
+		console.warn("requestNewDiagram dispatch not trusted.");
 	}
+
+	let diagramClass = diagramClasses[e.target.value];
+
+	currentProcessor = await diagramClass.createProcessor(currentChart, dataFolder);
+	await currentProcessor.drawChart();
 
 	console.log(e);
 	console.log(dataFolder);
 }
 
 async function dataSelectorClicked(e) {
-	let folder;
 	try {
-		folder = await window.showDirectoryPicker();
+		dataFolder = await window.showDirectoryPicker();
 		e.target.style.backgroundColor = "#0F0";
 	}
-	catch (e) {
-		if (e instanceof DOMException) {
-			e.target.style.backgroundColor = "#F00";
-			return;
-		}
+	catch (exception) {
+		e.target.style.backgroundColor = "#F00";
+		throw exception;
 	}
 
-	dataFolder = folder;
-
-	let calculations = {
-		addInHere: function (stuffToAdd) {
-			for (let [key, value] of Object.entries(stuffToAdd)) {
-				if (typeof value == "object") {
-					if (!this[key]) {
-						this[key] = { addInHere: this.addInHere };
-					}
-					this[key].addInHere(value);
-				}
-				else if (typeof value == "number") {
-					if (!this[key]) {
-						this[key] = value;
-						continue;
-					}
-					this[key] += value;
-				}
-			}
-		}
-	};
-
-	for await (const fileHandle of folder.values()) {
-		if (!fileHandle.name.startsWith("endsong_")) continue;
-		let file = await fileHandle.getFile();
-
-		let entriesObject = JSON.parse(await file.text());
-
-		let statsObtained = getPartialStats(entriesObject, true);
-		calculations.addInHere(statsObtained);
-	}
-
-	let totalStreams = 0;
-
-	for (let deviceStreams of Object.values(calculations["devicesUsed"])) {
-		if (typeof deviceStreams != "number") continue;
-		totalStreams += deviceStreams;
-	}
-
-	let newAddition = {
-		"devicesUsedShare": {}
-	};
-
-	for (let [deviceName, deviceAmount] of Object.entries(calculations["devicesUsed"])) {
-		newAddition["devicesUsedShare"][deviceName] = deviceAmount / totalStreams;
-	}
-
-	calculations.addInHere(newAddition);
-
-	console.log(calculations);
+	let diagramChooser = document.querySelector("#diagramChooser");
+	let changeEvent = new Event("change");
+	diagramChooser.dispatchEvent(changeEvent);
 }
