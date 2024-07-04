29eb532a12bbbd0bebb9d5ae2cf98726e2750b8a
BerndTheRipper <40756740+BerndTheRipper@users.noreply.github.com>
2024-07-04 23:56:27 +0200

    got started on oAuth Authentication might move that to separate repository

---
+++ js/oauth.js
@@ -0,0 +1,47 @@
+class Oauth {
+	authenticationURL;
+	clientID;
+	clientSecret;
+	redirectURL;
+	responseType;
+	scope;
+	#codeChallengeMethod = "S256";
+	#randomString;
+	#codeChallenge;
+	state;
+
+	/**
+	 * 
+	 */
+	constructor(appName = null, authenticationURL = null, clientID = null, clientSecret = null, redirectURL = null, state = null) {
+		this.#randomString = Oauth.generateRandomString(128);
+		let hashed = Oauth.generateHash(randomString);
+		this.#codeChallenge = Oauth.base64Encode(hashed);
+
+		if (appName == "spotify") {
+			//TODO make this a prefab that only gets iterated through
+			this.authenticationURL = "https://accounts.spotify.com/authorize";
+			this.clientID = "TBD";
+			this.redirectURL = location.origin + "/oAuthCallback";
+			this.responseType = "code";
+			//TODO check if this scope allows the creation of a private playlist
+			this.scope = "playlist-modify-public";
+			this.state = state;
+		}
+	}
+
+	static generateRandomString(length) {
+		//TODO write function
+		return "randomString";
+	}
+
+	static generateHash(plainText) {
+		//TODO write function
+		return "hashed";
+	}
+
+	static base64Encode(input) {
+		//TODO write function
+		return "base64";
+	}
+}
